cd0d087933429acb21c9e8b1a496133f360b1a89
ABeltramo <beltramo.ale@gmail.com>
2021-08-07 09:14:39 +0100

    retroarch: included assets and pre-installed cores

--- images/retroarch/scripts/startup.sh
+++ images/retroarch/scripts/startup.sh
@@ -15,8 +15,10 @@ LOG "Starting RetroArch with DISPLAY=${DISPLAY} and LOG_LEVEL=${LOG_LEVEL}"
 ensure-groups ${GOW_REQUIRED_DEVICES:-/dev/uinput /dev/input/event*}
 
 # Copying config in case it's the first time we mount from the host
-mkdir -p $HOME/retroarch/
+mkdir -p $HOME/retroarch/libretro/
 cp -u /cfg/retroarch.cfg $HOME/retroarch/retroarch.cfg
+# Copy pre-installed cores from the retroarch ppa
+cp -u /usr/lib/x86_64-linux-gnu/libretro/* /home/retro/retroarch/libretro/
 
 # Start Retroarch. Use `sudo` to make sure that group membership gets reloaded
 exec sudo -u $(whoami) -E /usr/bin/retroarch \
