bcbe684c289408df884f3e749dfdfbfcbc7cf497
Zach Bean <forty2@gmail.com>
2022-09-03 18:51:42 -0500

    Use nsenter and CAP_NET_ADMIN for udev instead of privileged mode

--- images/udevd/scripts/startup.sh
+++ images/udevd/scripts/startup.sh
@@ -4,9 +4,9 @@ set -e
 function start_udev() {
 	# mount_dev
     if command -v udevd &>/dev/null; then
-        unshare --net udevd --daemon &> /dev/null
+        nsenter udevd --daemon &> /dev/null
     else
-        unshare --net /lib/systemd/systemd-udevd --daemon &> /dev/null
+        nsenter /lib/systemd/systemd-udevd --daemon &> /dev/null
     fi
     udevadm trigger &> /dev/null
 }
