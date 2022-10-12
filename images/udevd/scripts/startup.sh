eb7dbb66fec93e5a61c202d757983ba375f58d6d
Zach Bean <forty2@gmail.com>
2022-10-11 21:23:51 -0500

    fix running udevadm trigger, add a UDEVD_QUIET environment variable

--- images/udevd/scripts/startup.sh
+++ images/udevd/scripts/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 set -e
+
+source /opt/gow/bash-lib/utils.sh
 
 function start_udev() {
 	# mount_dev
@@ -8,8 +11,14 @@ function start_udev() {
     else
         nsenter /lib/systemd/systemd-udevd --daemon &> /dev/null
     fi
-    udevadm trigger &> /dev/null
+    udevadm trigger &> /dev/null || true
 }
 
 start_udev
+
+if [ "${UDEVD_QUIET:-false}" = "true" ]; then
+    # redirect stdout to /dev/null before running udevadm monitor
+    exec >/dev/null
+fi
+
 exec udevadm monitor
