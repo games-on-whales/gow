d017e20d01e47f52c16f222fa4c35fa169cc17cb
Josh.5 <jsunnex@gmail.com>
2022-10-15 20:08:40 +1300

    Remove 'USER_PASSWORD' from base image
    We never need to set the default user password in these containers.
    I should not have added this.

--- images/base/overlay/etc/cont-init.d/10-setup_user.sh
+++ images/base/overlay/etc/cont-init.d/10-setup_user.sh
@@ -8,7 +8,6 @@ if [[ "${UNAME}" != "root" ]]; then
     PUID=${PUID:-1000}
     PGID=${PGID:-1000}
     UMASK=${UMASK:-000}
-    USER_PASSWORD=${USER_PASSWORD:-password}
 
     gow_log "Setting default user uid=${PUID}(${UNAME}) gid=${PGID}(${UNAME})"
     usermod -o -u "${PUID}" ${UNAME}
@@ -16,9 +15,6 @@ if [[ "${UNAME}" != "root" ]]; then
 
     gow_log "Setting umask to ${UMASK}";
     umask ${UMASK}
-
-    gow_log "Setting user password"
-    echo "${UNAME}:${USER_PASSWORD}" | chpasswd
 else
     gow_log "Container running as root. Nothing to do."
 fi
