4ef9731e44077af58be3a823ade5f6dc3759e2ff
ABeltramo <beltramo.ale@gmail.com>
2023-03-17 20:43:59 +0000

    feat: testing Gamescope

--- images/base-app/scripts/startup.sh
+++ images/base-app/scripts/startup.sh
@@ -4,8 +4,11 @@ set -e
 
 source /opt/gow/bash-lib/utils.sh
 
-gow_log "Waiting for X Server $DISPLAY to be available"
-/opt/gow/wait-x11
-
 # Launch the container's startup script
-exec /opt/gow/startup-app.sh
+if [ -z "$RUN_GAMESCOPE" ]; then
+    gow_log "Waiting for X Server $DISPLAY to be available"
+    /opt/gow/wait-x11
+    exec /opt/gow/startup-app.sh
+else
+    /usr/games/gamescope -b -- exec /opt/gow/startup-app.sh
+fi
