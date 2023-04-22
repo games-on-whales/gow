cfec0794e6aceab3983865e2d1ac8186c41f9dcd
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-04-22 17:23:11 +0700

    Some envs

--- images/emulationstationde/scripts/startup.sh
+++ images/emulationstationde/scripts/startup.sh
@@ -11,6 +11,8 @@ YUZU_CFG_DIR=$HOME/.local/share/yuzu
 XEMU_CFG_DIR=$HOME/.local/share/xemu
 PCSX2_CFG_DIR=$HOME/.config/PCSX2
 ES_CFG_DIR=$HOME/.emulationstation
+BIOSES_DIR=$HOME/bioses
+ROMS_DIR=$HOME/ROMs
 
 # Copying config in case it's the first time we mount from the host
 mkdir -p "$RA_CFG_DIR/cores/"
@@ -19,18 +21,18 @@ gow_log "Copying custom config - retroarch.cfg, if not edited"
 cp -u /cfg/retroarch.cfg "$RA_CFG_DIR/retroarch.cfg"
 
 gow_log "Copying custom config - ES-DE Custom Scripts Platform, if not edited"
-mkdir -p /home/retro/.emulationstation/custom_systems
-chown ${UNAME}:${UNAME} /home/retro/.emulationstation/custom_systems
-cp -u /cfg/es_systems.xml /home/retro/.emulationstation/custom_systems
+mkdir -p $ES_CFG_DIR/custom_systems
+chown ${UNAME}:${UNAME} $ES_CFG_DIR/custom_systems
+cp -u /cfg/es_systems.xml $ES_CFG_DIR/custom_systems
 
 gow_log "Copying custom config - RPCS3 Controller Bindings for Wolf, if not edited"
-cp -u /cfg/rpcs3/Default.yml /home/retro/.config/rpcs3/input_configs/global/Default.yml
+cp -u /cfg/rpcs3/Default.yml $RPCS3_CFG_DIR/input_configs/global/Default.yml
 
 gow_log "Copying custom config - PCSX2 settings, if not edited"
-cp -u /cfg/pcsx2/PCSX2.ini /home/retro/.config/PCSX2/inis/PCSX2.ini
+cp -u /cfg/pcsx2/PCSX2.ini $PCSX2_CFG_DIR/inis/PCSX2.ini
 
 gow_log "Copying custom config - XEMU settings, if not edited"
-cp -u /cfg/xemu/xemu.toml /home/retro/.local/share/xemu/xemu/xemu.toml
+cp -u /cfg/xemu/xemu.toml $XEMU_CFG_DIR/xemu/xemu.toml
 
 gow_log "Copying keys for YUZU if they are present or newer"
 if test -f $HOME/bioses/prod.keys; then
@@ -39,14 +41,14 @@ if test -f $HOME/bioses/prod.keys; then
 fi
 
 gow_log "Copying custom launch scripts for emulators"
-mkdir -p /home/retro/.emulationstation/custom_scripts
-chown ${UNAME}:${UNAME} /home/retro/.emulationstation/custom_scripts
-cp -u /cfg/retroarch.sh /home/retro/.emulationstation/custom_scripts/Launch_Retroarch.sh
-cp -u /cfg/rpcs3.sh /home/retro/.emulationstation/custom_scripts/Launch_rpcs3.sh
-cp -u /cfg/!Install_RPCS3_Firmware.sh /home/retro/.emulationstation/custom_scripts/!Install_RPCS3_Firmware.sh
-cp -u /cfg/yuzu.sh /home/retro/.emulationstation/custom_scripts/Launch_yuzu.sh
-cp -u /cfg/pcsx2.sh /home/retro/.emulationstation/custom_scripts/Launch_pcsx2.sh
-cp -u /cfg/xemu.sh /home/retro/.emulationstation/custom_scripts/Launch_xemu.sh
+mkdir -p $ES_CFG_DIR/custom_scripts
+chown ${UNAME}:${UNAME} $ES_CFG_DIR/custom_scripts
+cp -u /cfg/retroarch.sh $ES_CFG_DIR/custom_scripts/Launch_Retroarch.sh
+cp -u /cfg/rpcs3.sh $ES_CFG_DIR/custom_scripts/Launch_rpcs3.sh
+cp -u /cfg/!Install_RPCS3_Firmware.sh $ES_CFG_DIR/custom_scripts/!Install_RPCS3_Firmware.sh
+cp -u /cfg/yuzu.sh $ES_CFG_DIR/custom_scripts/Launch_yuzu.sh
+cp -u /cfg/pcsx2.sh $ES_CFG_DIR/custom_scripts/Launch_pcsx2.sh
+cp -u /cfg/xemu.sh $ES_CFG_DIR/custom_scripts/Launch_xemu.sh
 
 # if there are no cores, copy from the retroarch ppa
 # shellcheck disable=SC2046
@@ -60,16 +62,16 @@ if [ ! -d "$RA_CFG_DIR/assets" ]; then
 fi
 
 gow_log "Installing AppImage Emulators"
-mkdir -p /home/retro/Applications
-chown ${UNAME}:${UNAME} /home/retro/Applications
-cp -u /tmp/yuzu-emu.AppImage /home/retro/Applications/yuzu-emu.AppImage
-chmod a+x /home/retro/Applications/yuzu-emu.AppImage	
-cp -u /tmp/rpcs3-emu.AppImage /home/retro/Applications/rpcs3-emu.AppImage
-chmod a+x /home/retro/Applications/rpcs3-emu.AppImage
+mkdir -p $HOME/Applications
+chown ${UNAME}:${UNAME} $HOME/Applications
+cp -u /tmp/yuzu-emu.AppImage $HOME/Applications/yuzu-emu.AppImage
+chmod a+x $HOME/Applications/yuzu-emu.AppImage	
+cp -u /tmp/rpcs3-emu.AppImage $HOME/Applications/rpcs3-emu.AppImage
+chmod a+x $HOME/Applications/rpcs3-emu.AppImage
 
 gow_log "777 permissions on necessary folder"
-mkdir -p /home/retro/.local/share/yuzu/keys/
-chmod 777 /home/retro/.local/share/yuzu/keys/
+mkdir -p $YUZU_CFG_DIR/keys/
+chmod 777 $YUZU_CFG_DIR/keys/
 
 gow_log "Launching with Gamescope"
 chown ${UNAME}:${UNAME} /usr/games/gamescope
