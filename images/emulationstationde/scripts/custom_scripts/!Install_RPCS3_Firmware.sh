33a0f027abc409f701aa47d1062739f27f0d12ca
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-04-22 17:13:00 +0700

    Single BIOS location for RA/XEMU/YUZU/RPCS3/PCSX2

--- images/emulationstationde/scripts/custom_scripts/!Install_RPCS3_Firmware.sh
+++ images/emulationstationde/scripts/custom_scripts/!Install_RPCS3_Firmware.sh
@@ -3,4 +3,4 @@ set -e
 source /opt/gow/bash-lib/utils.sh
 
 gow_log "Install RPCS3 firmware from bios folder"
-/home/retro/Applications/rpcs3-emu.AppImage --installfw /home/retro/rpcs3_hdd0/PS3UPDAT.PUP
+/home/retro/Applications/rpcs3-emu.AppImage --installfw /home/retro/bioses/PS3UPDAT.PUP
--- images/emulationstationde/scripts/startup.sh
+++ images/emulationstationde/scripts/startup.sh
@@ -5,13 +5,18 @@ source /opt/gow/bash-lib/utils.sh
 
 gow_log "Starting Application"
 
-CFG_DIR=$HOME/.config/retroarch
+RA_CFG_DIR=$HOME/.config/retroarch
+RPCS3_CFG_DIR=$HOME/.config/rpcs3
+YUZU_CFG_DIR=$HOME/.local/share/yuzu
+XEMU_CFG_DIR=$HOME/.local/share/xemu
+PCSX2_CFG_DIR=$HOME/.config/PCSX2
+ES_CFG_DIR=$HOME/.emulationstation
 
 # Copying config in case it's the first time we mount from the host
-mkdir -p "$CFG_DIR/cores/"
+mkdir -p "$RA_CFG_DIR/cores/"
 
 gow_log "Copying custom config - retroarch.cfg, if not edited"
-cp -u /cfg/retroarch.cfg "$CFG_DIR/retroarch.cfg"
+cp -u /cfg/retroarch.cfg "$RA_CFG_DIR/retroarch.cfg"
 
 gow_log "Copying custom config - ES-DE Custom Scripts Platform, if not edited"
 mkdir -p /home/retro/.emulationstation/custom_systems
@@ -24,6 +29,15 @@ cp -u /cfg/rpcs3/Default.yml /home/retro/.config/rpcs3/input_configs/global/Defa
 gow_log "Copying custom config - PCSX2 settings, if not edited"
 cp -u /cfg/pcsx2/PCSX2.ini /home/retro/.config/PCSX2/inis/PCSX2.ini
 
+gow_log "Copying custom config - XEMU settings, if not edited"
+cp -u /cfg/xemu/xemu.toml /home/retro/.local/share/xemu/xemu/xemu.toml
+
+gow_log "Copying keys for YUZU if they are present or newer"
+if test -f $HOME/bioses/prod.keys; then
+    gow_log "YUZU keys are present, copy them to YUZU folder"
+    cp -u $HOME/bioses/prod.keys $YUZU_CFG_DIR/keys/prod.keys
+fi
+
 gow_log "Copying custom launch scripts for emulators"
 mkdir -p /home/retro/.emulationstation/custom_scripts
 chown ${UNAME}:${UNAME} /home/retro/.emulationstation/custom_scripts
@@ -34,15 +48,14 @@ cp -u /cfg/yuzu.sh /home/retro/.emulationstation/custom_scripts/Launch_yuzu.sh
 cp -u /cfg/pcsx2.sh /home/retro/.emulationstation/custom_scripts/Launch_pcsx2.sh
 cp -u /cfg/xemu.sh /home/retro/.emulationstation/custom_scripts/Launch_xemu.sh
 
-
 # if there are no cores, copy from the retroarch ppa
 # shellcheck disable=SC2046
-cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "$CFG_DIR/cores/"
+cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "$RA_CFG_DIR/cores/"
 
 # if there are no assets, manually download them
-if [ ! -d "$CFG_DIR/assets" ]; then
+if [ ! -d "$RA_CFG_DIR/assets" ]; then
     wget -q --show-progress -P /tmp https://buildbot.libretro.com/assets/frontend/assets.zip
-    7z x /tmp/assets.zip -bso0 -bse0 -bsp1 -o"$CFG_DIR/assets"
+    7z x /tmp/assets.zip -bso0 -bse0 -bsp1 -o"$RA_CFG_DIR/assets"
     rm /tmp/assets.zip
 fi
 
