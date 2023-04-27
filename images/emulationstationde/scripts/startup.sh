c369a773ae99c3feae44a03804f832ce4eb906f0
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-04-27 23:08:38 +0700

    folder cleaning

--- images/emulationstationde/scripts/startup.sh
+++ images/emulationstationde/scripts/startup.sh
@@ -19,12 +19,12 @@ APP_DIR=$HOME/Applications
 
 gow_log "Copying custom config - retroarch.cfg, if not edited"
 mkdir -p "$RA_CFG_DIR/cores/"
-cp -u /cfg/retroarch.cfg "$RA_CFG_DIR/retroarch.cfg"
+cp -u /cfg/retroarch/retroarch.cfg "$RA_CFG_DIR/retroarch.cfg"
 
 gow_log "Copying custom config - ES-DE Custom Scripts Platform, if not edited"
 mkdir -p $ES_CFG_DIR/custom_systems
 chown ${UNAME}:${UNAME} $ES_CFG_DIR/custom_systems
-cp -u /cfg/es_systems.xml $ES_CFG_DIR/custom_systems
+cp -u /cfg/es/es_systems.xml $ES_CFG_DIR/custom_systems
 
 gow_log "Copying custom config - RPCS3 Controller Bindings for Wolf and disable Auto-Update pop-up, if not edited"
 mkdir -p $RPCS3_CFG_DIR/input_configs/global/
@@ -54,20 +54,21 @@ cp -u /cfg/yuzu/qt-config.ini $YUZU_CFG_DIR2/qt-config.ini
 gow_log "Copying custom launch scripts for emulators, if not edited"
 mkdir -p $ES_CFG_DIR/custom_scripts
 chown ${UNAME}:${UNAME} $ES_CFG_DIR/custom_scripts
-cp -u /cfg/retroarch.sh $ES_CFG_DIR/custom_scripts/Launch_Retroarch.sh
-cp -u /cfg/winecfg.sh $ES_CFG_DIR/custom_scripts/Launch_Winecfg.sh
-cp -u /cfg/rpcs3.sh $ES_CFG_DIR/custom_scripts/Launch_rpcs3.sh
-cp -u /cfg/!Install_RPCS3_Firmware.sh $ES_CFG_DIR/custom_scripts/!Install_RPCS3_Firmware.sh
-cp -u /cfg/yuzu.sh $ES_CFG_DIR/custom_scripts/Launch_yuzu.sh
-cp -u /cfg/pcsx2.sh $ES_CFG_DIR/custom_scripts/Launch_pcsx2.sh
-cp -u /cfg/xemu.sh $ES_CFG_DIR/custom_scripts/Launch_xemu.sh
-cp -u /cfg/steam.sh $ES_CFG_DIR/custom_scripts/Launch_Steam.sh
+cp -u /cfg/custom_scripts/retroarch.sh $ES_CFG_DIR/custom_scripts/Launch_Retroarch.sh
+cp -u /cfg/custom_scripts/winecfg.sh $ES_CFG_DIR/custom_scripts/Launch_Winecfg.sh
+cp -u /cfg/custom_scripts/rpcs3.sh $ES_CFG_DIR/custom_scripts/Launch_rpcs3.sh
+cp -u /cfg/custom_scripts/!Install_RPCS3_Firmware.sh $ES_CFG_DIR/custom_scripts/!Install_RPCS3_Firmware.sh
+cp -u /cfg/custom_scripts/yuzu.sh $ES_CFG_DIR/custom_scripts/Launch_yuzu.sh
+cp -u /cfg/custom_scripts/pcsx2.sh $ES_CFG_DIR/custom_scripts/Launch_pcsx2.sh
+cp -u /cfg/custom_scripts/xemu.sh $ES_CFG_DIR/custom_scripts/Launch_xemu.sh
+cp -u /cfg/custom_scripts/steam.sh $ES_CFG_DIR/custom_scripts/Launch_Steam.sh
 
 gow_log "Checking RA Cores presence, if none - install them from PPA"
 cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "$RA_CFG_DIR/cores/"
 
 gow_log "Checking RA Assets presence, if none - install them"
 if [ ! -d "$RA_CFG_DIR/assets" ]; then
+    gow_log "No assets found, starting install"
     wget -q -P /tmp https://buildbot.libretro.com/assets/frontend/assets.zip
     7z x /tmp/assets.zip -bso0 -bse0 -bsp1 -o"$RA_CFG_DIR/assets"
     rm /tmp/assets.zip
