ff81db0e5903e32d4f8f7ab4e1178c80c326525d
ABeltramo <beltramo.ale@gmail.com>
2023-06-02 19:58:57 +0100

    fix: steam and retroarch

--- images/retroarch/scripts/startup.sh
+++ images/retroarch/scripts/startup.sh
@@ -14,7 +14,7 @@ cp -u /cfg/retroarch.cfg "$CFG_DIR/retroarch.cfg"
 
 # Copy pre-installed cores from the retroarch ppa
 # shellcheck disable=SC2046
-cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "$CFG_DIR/cores/"
+# cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "$CFG_DIR/cores/"
 
 # if there are no assets, manually download them
 if [ ! -d "$CFG_DIR/assets" ]; then
--- images/steam/scripts/startup.sh
+++ images/steam/scripts/startup.sh
@@ -8,12 +8,14 @@ gow_log "Starting Steam with DISPLAY=${DISPLAY}"
 # Recursively creating Steam necessary folders (https://github.com/ValveSoftware/steam-for-linux/issues/6492)
 mkdir -p "$HOME/.steam/ubuntu12_32/steam-runtime"
 
+STEAM_STARTUP_FLAGS=${STEAM_STARTUP_FLAGS:-"-oldbigpicture -bigpicture"}
+
 if [ -n "$RUN_GAMESCOPE" ]; then
   GAMESCOPE_WIDTH=${GAMESCOPE_WIDTH:-1920}
   GAMESCOPE_HEIGHT=${GAMESCOPE_HEIGHT:-1080}
   GAMESCOPE_REFRESH=${GAMESCOPE_REFRESH:-60}
   GAMESCOPE_MODE=${GAMESCOPE_MODE:-"-b"}
-  /usr/games/gamescope ${GAMESCOPE_MODE} -W ${GAMESCOPE_WIDTH} -H ${GAMESCOPE_HEIGHT} -r ${GAMESCOPE_REFRESH} -- /usr/games/steam -oldbigpicture -bigpicture
+  /usr/games/gamescope ${GAMESCOPE_MODE} -W ${GAMESCOPE_WIDTH} -H ${GAMESCOPE_HEIGHT} -r ${GAMESCOPE_REFRESH} -- /usr/games/steam ${STEAM_STARTUP_FLAGS}
 else
- exec /usr/games/steam
+ exec /usr/games/steam ${STEAM_STARTUP_FLAGS}
 fi
