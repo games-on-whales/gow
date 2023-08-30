6bdf3e738cf504836a715a3da51de8bfdd71ccc7
Victoria Brekenfeld <github@drakulix.de>
2023-08-30 20:46:09 +0000

    steam: Use new big picture mode by default

--- images/steam/scripts/startup.sh
+++ images/steam/scripts/startup.sh
@@ -8,7 +8,7 @@ gow_log "Starting Steam with DISPLAY=${DISPLAY}"
 # Recursively creating Steam necessary folders (https://github.com/ValveSoftware/steam-for-linux/issues/6492)
 mkdir -p "$HOME/.steam/ubuntu12_32/steam-runtime"
 
-STEAM_STARTUP_FLAGS=${STEAM_STARTUP_FLAGS:-"-oldbigpicture -bigpicture"}
+STEAM_STARTUP_FLAGS=${STEAM_STARTUP_FLAGS:-"-bigpicture -steamos"}
 
 dbus-daemon --session --fork
 
@@ -16,9 +16,9 @@ if [ -n "$RUN_GAMESCOPE" ]; then
   GAMESCOPE_WIDTH=${GAMESCOPE_WIDTH:-1920}
   GAMESCOPE_HEIGHT=${GAMESCOPE_HEIGHT:-1080}
   GAMESCOPE_REFRESH=${GAMESCOPE_REFRESH:-60}
-  GAMESCOPE_MODE=${GAMESCOPE_MODE:-"-b"}
+  GAMESCOPE_MODE=${GAMESCOPE_MODE:-"-b -e"}
   # shellcheck disable=SC2086
-  /usr/games/gamescope "${GAMESCOPE_MODE}" -W "${GAMESCOPE_WIDTH}" -H "${GAMESCOPE_HEIGHT}" -r "${GAMESCOPE_REFRESH}" -- /usr/games/steam ${STEAM_STARTUP_FLAGS}
+  /usr/games/gamescope ${GAMESCOPE_MODE} -W "${GAMESCOPE_WIDTH}" -H "${GAMESCOPE_HEIGHT}" -r "${GAMESCOPE_REFRESH}" -- /usr/games/steam ${STEAM_STARTUP_FLAGS}
 else
   # shellcheck disable=SC2086
   exec /usr/games/steam ${STEAM_STARTUP_FLAGS}
