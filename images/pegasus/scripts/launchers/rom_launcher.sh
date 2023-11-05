e402958e6d3b6336e6fb6d63dd6fb0a9f807f02b
Julien Pierret <dafrenchyman@gmail.com>
2023-11-05 09:42:44 -0800

    Dolphin sort-of working

--- images/pegasus/scripts/launchers/rom_launcher.sh
+++ images/pegasus/scripts/launchers/rom_launcher.sh
@@ -7,7 +7,7 @@ declare -A EMULATOR_COMMAND=( \
 ["gb"]="retroarch --fullscreen -L ~/.config/retroarch/cores/gambatte_libretro.so "${ROM}"" \
 ["gbc"]="retroarch --fullscreen -L ~/.config/retroarch/cores/gambatte_libretro.so "${ROM}"" \
 ["gba"]="retroarch --fullscreen -L ~/.config/retroarch/cores/mgba_libretro.so \"${ROM}\"" \
-["gc"]="/usr/games/dolphin-emu --batch --exec=${ROM} " \
+["gc"]="/Applications/dolphin-emu.AppImage --appimage-extract-and-run --batch --exec=\"${ROM}\"" \
 ["genesis"]="retroarch --fullscreen -L ~/.config/retroarch/cores/picodrive_libretro.so \"${ROM}\"" \
 ["nes"]="retroarch --fullscreen -L ~/.config/retroarch/cores/fceumm_libretro.so \"${ROM}\"" \
 ["psx"]="retroarch --fullscreen -L ~/.config/retroarch/cores/pcsx_rearmed_libretro.so \"${ROM}\"" \
@@ -15,6 +15,7 @@ declare -A EMULATOR_COMMAND=( \
 ["sega32x"]="retroarch --fullscreen -L ~/.config/retroarch/cores/picodrive_libretro.so \"${ROM}\"" \
 ["snes"]="retroarch --fullscreen -L ~/.config/retroarch/cores/bsnes_hd_beta_libretro.so \"${ROM}\"" \
 )
+# ["gc"]="retroarch --fullscreen -L ~/.config/retroarch/cores/dolphin_libretro.so \"${ROM}\"" \
 
 echo "Running command: ${EMULATOR_COMMAND[${EMULATOR}]}"
 eval ${EMULATOR_COMMAND[${EMULATOR}]}
--- images/pegasus/setup.sh
+++ images/pegasus/setup.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge" .
