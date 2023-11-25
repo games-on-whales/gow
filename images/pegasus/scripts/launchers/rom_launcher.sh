909684a1a2477fc3625032474a019769a0aa46b7
Julien Pierret <dafrenchyman@gmail.com>
2023-11-25 10:25:12 -0800

    Added PS2 support.

--- images/pegasus/scripts/launchers/rom_launcher.sh
+++ images/pegasus/scripts/launchers/rom_launcher.sh
@@ -4,6 +4,7 @@ ROM=$2
 
 # dictionary of emulator to command to run
 declare -A EMULATOR_COMMAND=( \
+["arcade"]="retroarch --fullscreen -L ~/.config/retroarch/cores/mame_libretro.so \"${ROM}\"" \
 ["atari2600"]="retroarch --fullscreen -L ~/.config/retroarch/cores/stella_libretro.so \"${ROM}\"" \
 ["gb"]="retroarch --fullscreen -L ~/.config/retroarch/cores/gambatte_libretro.so \"${ROM}\"" \
 ["gbc"]="retroarch --fullscreen -L ~/.config/retroarch/cores/gambatte_libretro.so \"${ROM}\"" \
@@ -15,12 +16,12 @@ declare -A EMULATOR_COMMAND=( \
 ["ngp"]="retroarch --fullscreen -L ~/.config/retroarch/cores/mednafen_ngp_libretro.so \"${ROM}\"" \
 ["ngpc"]="retroarch --fullscreen -L ~/.config/retroarch/cores/mednafen_ngp_libretro.so \"${ROM}\"" \
 ["psx"]="retroarch --fullscreen -L ~/.config/retroarch/cores/pcsx_rearmed_libretro.so \"${ROM}\"" \
-["ps2"]="retroarch --fullscreen -L ~/.config/retroarch/cores/pcsx2_libretro.so \"${ROM}\"" \
+["ps2"]="/Applications/pcsx2-emu-Qt.AppImage --appimage-extract-and-run \"${ROM}\"" \
 ["sega32x"]="retroarch --fullscreen -L ~/.config/retroarch/cores/picodrive_libretro.so \"${ROM}\"" \
 ["snes"]="retroarch --fullscreen -L ~/.config/retroarch/cores/bsnes_hd_beta_libretro.so \"${ROM}\"" \
+["wii"]="/Applications/dolphin-emu.AppImage --appimage-extract-and-run --batch --exec=\"${ROM}\"" \
 ["xbox"]="/Applications/xemu-emu.AppImage --appimage-extract-and-run -full-screen -dvd_path \"${ROM}\"" \
 )
-# ["gc"]="retroarch --fullscreen -L ~/.config/retroarch/cores/dolphin_libretro.so \"${ROM}\"" \
 
 echo "Running command: ${EMULATOR_COMMAND[${EMULATOR}]}"
 eval ${EMULATOR_COMMAND[${EMULATOR}]}
--- images/pegasus/setup.sh
+++ images/pegasus/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge" .
+# sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge" .
+
+sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:sha-b979a7c" .
