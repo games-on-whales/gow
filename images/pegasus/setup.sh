93991316e629c669bcc41b447747921d0e21de64
Julien Pierret <dafrenchyman@gmail.com>
2023-12-08 08:08:12 -0800

    Added more emulators to the mix

--- images/pegasus/setup.sh
+++ images/pegasus/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge" .
+# sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge" .
 
-# sudo docker build -t gow/pegasus:latest --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:sha-b979a7c" .
+sudo docker build -t gow/pegasus:sha-b979a7c --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:sha-b979a7c" .
+
+# sudo docker build -t gow/pegasus:sha-0b348a5 --build-arg="BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:sha-0b348a5" .
