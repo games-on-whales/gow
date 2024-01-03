adef294498fb778a1e9fe78cf7fab5e636fde970
Julien Pierret <dafrenchyman@gmail.com>
2024-01-02 22:46:49 -0800

    Clean-up Working on sway
    - Added sway as a choice if gamescope not selected
    - Cleaned up and got cemu working
    - Removed mesa-vulkan-drivers in containers that were installing it as it's already part of the base image.
    - Cleaned up a lot of different things.
    - Made setting up xbox controllers an optional script.

--- images/pegasus/scripts/launchers/xemu.sh
+++ images/pegasus/scripts/launchers/xemu.sh
@@ -5,4 +5,3 @@ source /opt/gow/bash-lib/utils.sh
 gow_log "Starting XEMU with DISPLAY=${DISPLAY}"
 cd /Applications
 ./xemu-emu.AppImage --appimage-extract-and-run
-#xemu
--- images/pegasus/scripts/startup.sh
+++ images/pegasus/scripts/startup.sh
@@ -12,13 +12,14 @@ ln -sf /bioses $HOME
 PEGASUS_CFG=$HOME/.config/pegasus-frontend
 gow_log "Pegasus - Configure"
 mkdir -p "$PEGASUS_CFG"
-gow_log "Dolphin - Copying game_dirs.txt, if not edited"
+gow_log "Pegasus - Copying game_dirs.txt, if not edited"
 cp -u /cfg/app/game_dirs.txt "${PEGASUS_CFG}/game_dirs.txt"
 
 #########################################
 # Configure PCSX2
 #########################################
 PCSX2_CFG=$HOME/.config/PCSX2
+
 gow_log "PCSX2 - Configure"
 mkdir -p "$PCSX2_CFG/inis/"
 cp -u /cfg/PCSX2/PCSX2.ini "${PCSX2_CFG}/inis/PCSX2.ini"
@@ -27,11 +28,10 @@ cp -u /cfg/PCSX2/PCSX2.ini "${PCSX2_CFG}/inis/PCSX2.ini"
 # Configure Dolphin
 #########################################
 DOLPHIN_CFG=$HOME/.config/dolphin-emu
+
 mkdir -p "$DOLPHIN_CFG"
 gow_log "Dolphin - Copying config, if not edited"
 cp -u /cfg/dolphin/Dolphin.ini "$DOLPHIN_CFG/Dolphin.ini"
-gow_log "Dolphin - Copying controller config, if not edited"
-cp -u /cfg/dolphin/GCPadNew.ini "$DOLPHIN_CFG/GCPadNew.ini"
 
 #########################################
 # Configure Retroarch
@@ -59,7 +59,7 @@ fi
 # Configure Yuzu
 #########################################
 YUZU_CFG_DIR=$HOME/.local/share/yuzu
-YUZU_CFG_DIR2=$HOME/.config/yuzu
+
 gow_log "YUZU - Copying pred/title keys for YUZU"
 if test -f /bioses/prod.keys; then
     gow_log "YUZU - /bioses/prod.keys present, copy to YUZU folder"
@@ -77,16 +77,40 @@ mkdir -p $YUZU_CFG_DIR2
 cp -u /cfg/yuzu/qt-config.ini $YUZU_CFG_DIR2/qt-config.ini
 
 #########################################
+# Configure CEMU
+#########################################
+CEMU_CFG_DIR=$HOME/.config/Cemu
+
+gow_log "CEMU - Setting default sound device and run in fullscreen, if not edited"
+cp /cfg/cemu/settings.xml /cfg/cemu/settings_new.xml
+searchString="<TVDevice>replace_me</TVDevice>"
+replaceString="<TVDevice>${PULSE_SINK}</TVDevice>"
+sed -i -e "s|$searchString|$replaceString|g" /cfg/cemu/settings_new.xml
+searchString="<PadDevice>replace_me</PadDevice>"
+replaceString="<PadDevice>${PULSE_SINK}</PadDevice>"
+sed -i -e "s|$searchString|$replaceString|g" /cfg/cemu/settings_new.xml
+# Set the date of the file back to the original date
+touch -r /cfg/cemu/settings.xml /cfg/cemu/settings_new.xml
+cp -u /cfg/cemu/settings_new.xml $CEMU_CFG_DIR/settings.xml
+
+#########################################
 # Configure RPCS3
 #########################################
 RPCS3_CFG_DIR=$HOME/.config/rpcs3
-gow_log "RPCS3 - Copying controller bindings for Wolf, if not edited"
-mkdir -p $RPCS3_CFG_DIR/input_configs/global/
-cp -u /cfg/rpcs3/Default.yml $RPCS3_CFG_DIR/input_configs/global/Default.yml
 
 gow_log "RPCS3 - Copying custom config (disable Auto-Update pop-up), if not edited"
 mkdir -p $RPCS3_CFG_DIR/GuiConfigs/
 cp -u /cfg/rpcs3/CurrentSettings.ini $RPCS3_CFG_DIR/GuiConfigs/CurrentSettings.ini
+
+gow_log "RPCS3 - Setting default sound device and run in fullscreen, if not edited"
+mkdir -p $RPCS3_CFG_DIR/
+cp /cfg/rpcs3/config.yml /cfg/rpcs3/config_new.yml
+searchString="Audio Device: \"@@@default@@@\""
+replaceString="Audio Device: \"${PULSE_SINK}\""
+sed -i -e "s|$searchString|$replaceString|g" /cfg/rpcs3/config_new.yml
+# Set the date of the file back to the original date
+touch -r /cfg/rpcs3/config.yml /cfg/rpcs3/config_new.yml
+cp -u /cfg/rpcs3/config_new.yml $RPCS3_CFG_DIR/config.yml
 
 #########################################
 # Configure XEMU
@@ -108,5 +132,9 @@ if [ -n "$RUN_GAMESCOPE" ]; then
   export GAMESCOPE_MODE=${GAMESCOPE_MODE:-"-b"}
   /usr/games/gamescope "${GAMESCOPE_MODE}" -W "${GAMESCOPE_WIDTH}" -H "${GAMESCOPE_HEIGHT}" -r "${GAMESCOPE_REFRESH}" -- pegasus-fe
 else
-  exec pegasus-fe
+  echo "Sway - Starting Pegasus"
+  export XDG_SESSION_TYPE=wayland
+  mkdir -p $HOME/.config/sway/
+  cp /cfg/sway/config $HOME/.config/sway/config
+  exec sway --unsupported-gpu
 fi
