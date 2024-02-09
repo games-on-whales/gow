729234089143112e4952a155f3ee7146fdfe6db9
Julien Pierret <dafrenchyman@gmail.com>
2024-02-08 18:26:15 -0800

    Fixed issue with resolutions in sway, updated some emulators

--- images/base-app/scripts/launch-comp.sh
+++ images/base-app/scripts/launch-comp.sh
@@ -2,12 +2,13 @@
 
 function launcher() {
   APP_TO_LAUNCH=$1
+  export GAMESCOPE_WIDTH=${GAMESCOPE_WIDTH:-1920}
+  export GAMESCOPE_HEIGHT=${GAMESCOPE_HEIGHT:-1080}
+  export GAMESCOPE_REFRESH=${GAMESCOPE_REFRESH:-60}
 
   if [ -n "$RUN_GAMESCOPE" ]; then
     echo "Gamescope - Starting Pegasus"
-    export GAMESCOPE_WIDTH=${GAMESCOPE_WIDTH:-1920}
-    export GAMESCOPE_HEIGHT=${GAMESCOPE_HEIGHT:-1080}
-    export GAMESCOPE_REFRESH=${GAMESCOPE_REFRESH:-60}
+
     export GAMESCOPE_MODE=${GAMESCOPE_MODE:-"-b"}
     /usr/games/gamescope "${GAMESCOPE_MODE}" -W "${GAMESCOPE_WIDTH}" -H "${GAMESCOPE_HEIGHT}" -r "${GAMESCOPE_REFRESH}" -- ${APP_TO_LAUNCH}
   else
@@ -15,7 +16,8 @@ function launcher() {
     export XDG_SESSION_TYPE=wayland
     mkdir -p $HOME/.config/sway/
     cp /cfg/sway/config $HOME/.config/sway/config
-    # Modify the config file to launch the ${APP_TO_LAUNCH} at the end
+    # Modify the config file for res and to launch the ${APP_TO_LAUNCH} at the end
+    echo "output * resolution ${GAMESCOPE_WIDTH}x${GAMESCOPE_HEIGHT} position ${GAMESCOPE_WIDTH},0" >> $HOME/.config/sway/config
     echo "workspace main; exec ${APP_TO_LAUNCH}" >> $HOME/.config/sway/config
     # Start sway
     exec sway --unsupported-gpu
