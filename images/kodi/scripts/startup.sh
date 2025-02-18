b71bbcc64984ab08b71f5310d8312cfd82b14157
Sebastian Sommer <mail@sommer-sebastian.de>
2025-02-19 00:33:31 +0100

    Kodi system wide install & cleanup

--- images/kodi/scripts/startup.sh
+++ images/kodi/scripts/startup.sh
@@ -11,10 +11,16 @@ for file in /opt/gow/startup.d/* ; do
     fi
 done
 
-gow_log "Installing Kodi"
 
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y --noninteractive tv.kodi.Kodi
+
+export XDG_DATA_DIRS=/var/lib/flatpak/exports/share:/home/retro/.local/share/flatpak/exports/share:/usr/local/share/:/usr/share/
+
+# user wide install of kodi
+#gow_log "Installing Kodi"
+#flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#flatpak install -y --noninteractive tv.kodi.Kodi
+#flatpak override --user tv.kodi.Kodi --filesystem=home
+#flatpak update -y --noninteractive
 
 gow_log "Starting Kodi"
 source /opt/gow/launch-comp.sh
