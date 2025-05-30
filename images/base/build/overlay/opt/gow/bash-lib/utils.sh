9f92828946a7062d16f6def2a5de5b4c34f1a246
shawly <shawlyde@gmail.com>
2025-05-30 21:00:44 +0200

    fix(base): add curl
    Necessary for /opt/gow/bash-lib/utils.sh to work, so it should be added to the base image.

--- images/base/build/overlay/opt/gow/bash-lib/utils.sh
+++ images/base/build/overlay/opt/gow/bash-lib/utils.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -u
 
 gow_log() {
     echo "$(date +"[%Y-%m-%d %H:%M:%S]") $*"
@@ -7,7 +9,7 @@ gow_log() {
 join_by() { local IFS="$1"; shift; echo "$*"; }
 
 github_download(){
-    curl -H "X-GitHub-Api-Version: "$(GITHUB_REST_VERSION:-2022-11-28)"" \
+    curl -H "X-GitHub-Api-Version: "${GITHUB_REST_VERSION:-2022-11-28}"" \
         https://api.github.com/repos/$1/releases/latest | \
         jq $2 | \
         xargs wget -O $3
