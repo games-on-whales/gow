def533aa81d691e010916f8d784c890c64600999
shawly <shawlyde@gmail.com>
2025-06-08 21:22:49 +0200

    feat(steam): allow shutdown through power menu
    steamos-dbus-watchdog.sh listens for dbus messages which are sent by Steam Big Picture's power menu and then sends a shutdown signal to steam using the -shutdown arg. This allows stopping using the power menu which is useful when using steam with -steamos/-steamdeck args.

--- apps/steam/build/scripts/system-services.sh
+++ apps/steam/build/scripts/system-services.sh
@@ -8,5 +8,8 @@ bluetoothd --nodetach &
 echo "*** Bluez started ***"
 NetworkManager
 echo "*** NetworkManager started ***"
+# Watchdog will stop steam when selecting Turn off, Suspend or Restart from the Steam power menu
+steamos-dbus-watchdog.sh &
+echo "*** D-Bus Watchdog started ***"
 
 disown
