d4c2239eff626a38a153a2d36e66b56d70309455
Robert Koszewski <rkkoszewski@gmail.com>
2025-07-03 21:12:34 +0200

    Add ROM launcher support

--- images/base-emu/build/launchers/xenia.sh
+++ images/base-emu/build/launchers/xenia.sh
@@ -5,6 +5,6 @@ source /opt/gow/bash-lib/utils.sh
 XENIA_WORKDIR="$HOME/.local/share/Xenia/root"
 mkdir -p "$XENIA_WORKDIR"
 
-gow_log "Starting Xenia Canary with DISPLAY=${DISPLAY} and working directory $XENIA_WORKDIR"
+gow_log "Starting Xenia Canary with DISPLAY=${DISPLAY} and working directory $XENIA_WORKDIR and args: $@"
 cd "$XENIA_WORKDIR"
-/Applications/xenia-canary/xenia_canary 
+/Applications/xenia-canary/xenia_canary "$@"
