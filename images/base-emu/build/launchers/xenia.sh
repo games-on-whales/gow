c79e080ff4433f52824591cade290b012cd36420
Robert Koszewski <rkkoszewski@gmail.com>
2025-07-03 21:04:47 +0200

    Add Xenia Canary to Pegasus and base-emu

---
+++ images/base-emu/build/launchers/xenia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+source /opt/gow/bash-lib/utils.sh
+
+XENIA_WORKDIR="$HOME/.local/share/Xenia/root"
+mkdir -p "$XENIA_WORKDIR"
+
+gow_log "Starting Xenia Canary with DISPLAY=${DISPLAY} and working directory $XENIA_WORKDIR"
+cd "$XENIA_WORKDIR"
+/Applications/xenia-canary/xenia_canary 
