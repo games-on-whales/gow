8eebbe67af458f0be3b2cd6a0ac4253c71a227e2
matdave <mat@matdave.com>
2025-12-20 19:10:42 -0600

    fix(bottles): update capadd

--- apps/bottles/build/scripts/startup.sh
+++ apps/bottles/build/scripts/startup.sh
@@ -11,17 +11,8 @@ for file in /opt/gow/startup.d/* ; do
     fi
 done
 
-
-
 export XDG_DATA_DIRS=/var/lib/flatpak/exports/share:/home/retro/.local/share/flatpak/exports/share:/usr/local/share/:/usr/share/
 
-# user wide install of kodi
-#gow_log "Installing Kodi"
-#flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-#flatpak install -y --noninteractive tv.kodi.Kodi
-#flatpak override --user tv.kodi.Kodi --filesystem=home
-#flatpak update -y --noninteractive
-
-gow_log "Starting Kodi"
+gow_log "Starting Bottles"
 source /opt/gow/launch-comp.sh
 launcher flatpak run com.usebottles.bottles
