fa85e47f305bdf7ee874ffd282f744e080d770e5
clsty <clsty@celestialy.top>
2026-04-12 18:07:33 +0800

    Use the latest gamepad-ui, enable SDL by default

--- apps/gamepad-ui/build/scripts/startup.sh
+++ apps/gamepad-ui/build/scripts/startup.sh
@@ -20,5 +20,11 @@ if [ -z "$RUN_GAMESCOPE" ] && [ -z "$RUN_SWAY" ]; then
 fi
 # Enable standalone UI behavior by default in container deployments.
 export STANDALONE_SESSION=${STANDALONE_SESSION:-1}
+# Workaround:
+# Enable SDL support cuz Electron Gamepad-API does not work
+# see https://github.com/games-on-whales/gow/pull/293#issuecomment-4150484665
+# This could lead to wrong button mapping for Dualsense controllers,
+# see https://github.com/games-on-whales/gow/pull/293#issuecomment-4169381777
+export LUTRIS_GAMEPAD_UI_ENABLE_SDL_INPUT=${LUTRIS_GAMEPAD_UI_ENABLE_SDL_INPUT:-1}
 
 launcher /opt/gow/app/AppRun
