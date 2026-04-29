8a968a4d53083a6fc7e083bb7c954d00f9fc5e18
clsty <celestial.y@outlook.com>
2026-04-30 01:00:52 +0800

    Fix typo

--- apps/lutris/build-fedora/scripts/startup.sh
+++ apps/lutris/build-fedora/scripts/startup.sh
@@ -23,7 +23,7 @@ if [ ${WOLF_LUTRIS_GAMEPAD_UI_ENABLE:-1} -eq 1 ]; then
     launcher /opt/gow/app/AppRun
 else
     if [ -z "$RUN_GAMESCOPE" ] && [ -z "$RUN_SWAY" ]; then
-        export RUN_SWaY=1
+        export RUN_SWAY=1
     fi
     launcher "${LUTRIS}" "${LUTRIS_ARGS[@]}"
 fi
